5a882d292fed0dc88b49efc256e31db4ec5e5529
Tom Ross <tromik@gmail.com>
2017-09-25 09:32:19 -0400

    Class math

--- numstring.py
+++ numstring.py
@@ -14,7 +14,29 @@ class NumString:
     def __add__(self, other):
         # five = 5
         # five + 4 = '54'
-        return self.value  + str(other)
+        # return self.value  + str(other)
+        # ^ not very helpful or useful
+        # if "." in self.value:
+        #     return float(self.value) + other
+        # return int(self.value) + other
+        # now NumString objects support mathematical additional
+        # this also works (no value)
+        if "." in self.value:
+            return float(self) + other
+        return int(self) + other
+        # this will only work when the NumString is on the LEFT of the equation
+        # __radd__ (reflect add) needed for other
+
+    def __radd__(self, other):
+        # if "." in self.value:
+        #     return other + float(self)
+        # return other + int(self)
+        # now NumString(5) + 4 and 4 + NumString(5) will work
+        # However, can also do this:
+        return self + other
+        # which will call __add_
+        # 4 + NumString(5) -- > __add__, fails, passed to __radd__ reflected which
+        #   uses __add__
 
 # >>> from numstring import NumString
 # >>> five = NumString(5)
